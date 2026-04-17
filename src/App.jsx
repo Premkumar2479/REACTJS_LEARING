b9bbec3caff43640c318c4f0c3c83e5ae3355f45
Premkumar2479 <pk2335301@gmail.com>
2026-04-17 22:20:05 +0530

    initial commit -m

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Hello from "./Hello"
+
+function App() {
+
+  return (
+    <>
+        <h1>HELLO WORLD!</h1>
+        <Hello />
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/Hello.jsx
@@ -0,0 +1,5 @@
+function Hello() {
+  return <h2>Hello Component Working!</h2>;
+}
+
+export default Hello;
